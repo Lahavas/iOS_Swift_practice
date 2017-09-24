8454577e12f9a284a9d2465b1229c89f02a37473
Lahavas <uv187sn@naver.com>
2017-09-24 14:03:21 +0900

    Ch04 update label and text field, dismiss keyboard action

--- Big_Nerd_Ranch/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ Big_Nerd_Ranch/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversionViewController.swift
 //  WorldTrotter
 //
 //  Created by Jaeho on 2017. 9. 20..
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversionViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: - Memory Management
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Actions
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ sender: UITextField) {
+        
+        if let text = sender.text, !text.isEmpty {
+            self.celsiusLabel.text = text
+        } else {
+            self.celsiusLabel.text = "???"
+        }
+    }
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+    
+        self.textField.resignFirstResponder()
+    }
 }
 
