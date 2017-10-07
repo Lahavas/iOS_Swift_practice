106076d06013e42dea0e2ab16b7d986e9f883871
Lahavas <uv187sn@naver.com>
2017-10-07 15:37:54 +0900

    Ch10 create Item object

---
+++ Big_Nerd_Ranch/Homepnwer/Homepnwer/Item.swift
@@ -0,0 +1,58 @@
+//
+//  Item.swift
+//  Homepnwer
+//
+//  Created by Jaeho on 2017. 10. 7..
+//  Copyright © 2017년 yeon. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    
+    // MARK: - Properties
+    
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    // MARK: - Initialization
+    
+    init(name: String, valueInDollars: Int, serialNumber: String?) {
+        
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = Date()
+        
+        super.init()
+    }
+    
+    convenience init(random: Bool = false) {
+        
+        if random {
+            
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            var index = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjective = adjectives[Int(index)]
+            
+            index = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(index)]
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber =
+                UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName,
+                      valueInDollars: randomValue,
+                      serialNumber: randomSerialNumber)
+        } else {
+            
+            self.init(name: "", valueInDollars: 0, serialNumber: nil)
+        }
+    }
+}
