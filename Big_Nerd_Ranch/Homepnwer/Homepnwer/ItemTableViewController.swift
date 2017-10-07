4d09906f2417b6534f317ea741a86a7e7b3d1c50
Lahavas <uv187sn@naver.com>
2017-10-07 15:27:21 +0900

    Ch10 create ItemTableViewController

--- Big_Nerd_Ranch/Homepnwer/Homepnwer/ItemTableViewController.swift
+++ Big_Nerd_Ranch/Homepnwer/Homepnwer/ItemTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ItemTableViewController.swift
 //  Homepnwer
 //
 //  Created by Jaeho on 2017. 10. 7..
@@ -8,17 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ItemTableViewController: UITableViewController {
+    
+    // MARK: - Properties
 
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    // MARK: - Memory Management
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
