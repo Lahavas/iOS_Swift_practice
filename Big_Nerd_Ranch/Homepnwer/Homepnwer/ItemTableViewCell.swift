08e7d0a301ba3efb94b77d6b2ba780912f3170ce
Lahavas <uv187sn@naver.com>
2017-10-08 12:57:32 +0900

    Ch12 add itemTableViewCell

---
+++ Big_Nerd_Ranch/Homepnwer/Homepnwer/ItemTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  ItemTableViewCell.swift
+//  Homepnwer
+//
+//  Created by Jaeho on 2017. 10. 8..
+//  Copyright © 2017년 yeon. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+
+    // MARK: - View Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
