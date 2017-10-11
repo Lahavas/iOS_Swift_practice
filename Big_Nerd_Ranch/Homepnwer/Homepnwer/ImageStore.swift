13cd367a76a7d126554828c3a9ecccda08da5b21
Lahavas <uv187sn@naver.com>
2017-10-11 16:32:52 +0900

    Ch16 save image

--- Big_Nerd_Ranch/Homepnwer/Homepnwer/ImageStore.swift
+++ Big_Nerd_Ranch/Homepnwer/Homepnwer/ImageStore.swift
@@ -17,14 +17,48 @@ class ImageStore {
     // MARK: - Methods
     
     func setImage(_ image: UIImage, forKey key: String) {
+        
         self.cache.setObject(image, forKey: key as NSString)
+        
+        let url = self.imageURL(forKey: key)
+        
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+            let _ = try? data.write(to: url, options: [.atomic])
+        }
     }
     
     func image(forKey key: String) -> UIImage?{
-        return self.cache.object(forKey: key as NSString)
+        
+        if let existingImage = self.cache.object(forKey: key as NSString) {
+            return existingImage
+        }
+        
+        let url = self.imageURL(forKey: key)
+        guard let imageFromDisk = UIImage(contentsOfFile: url.path) else {
+            return nil
+        }
+        
+        self.cache.setObject(imageFromDisk, forKey: key as NSString)
+        return imageFromDisk
     }
     
     func deleteImage(forKey key: String) {
         self.cache.removeObject(forKey: key as NSString)
+        
+        let url = imageURL(forKey: key)
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch let deleteError {
+            print("Error removing the image from disk: \(deleteError)")
+        }
+    }
+    
+    func imageURL(forKey key: String) -> URL {
+        
+        let documentsDirectories =
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        
+        return documentDirectory.appendingPathComponent(key)
     }
 }
